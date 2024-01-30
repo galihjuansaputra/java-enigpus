a6eb57f36d1a37b51717e16d78173fff3ffefa3a
Galih Prastya Juansaputra <galih.juansaputra@gmail.com>
2024-01-30 14:06:19 +0700

    add create magazine and viwe magazine feature

--- src/com/enigma/enigpus/entity/Magazine.java
+++ src/com/enigma/enigpus/entity/Magazine.java
@@ -1,19 +1,59 @@
 package com.enigma.enigpus.entity;
 
-public class Magazine extends Book{
+import java.io.Serial;
+import java.io.Serializable;
 
+public class Magazine extends Book implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
     String code;
     String title;
     String releasePer;
     Integer releaseYear;
 
+    public Magazine(String code, String title, String releasePer, Integer releaseYear) {
+        this.code = code;
+        this.title = title;
+        this.releasePer = releasePer;
+        this.releaseYear = releaseYear;
+    }
+
+    public Magazine(){
+
+    }
+
     @Override
     public String getCode() {
-        return null;
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 
     @Override
     public String getTitle() {
-        return null;
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleasePer() {
+        return releasePer;
+    }
+
+    public void setReleasePer(String releasePer) {
+        this.releasePer = releasePer;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
     }
 }
--- src/com/enigma/enigpus/view/BookView.java
+++ src/com/enigma/enigpus/view/BookView.java
@@ -1,6 +1,7 @@
 package com.enigma.enigpus.view;
 
 import com.enigma.enigpus.entity.Book;
+import com.enigma.enigpus.entity.Magazine;
 import com.enigma.enigpus.entity.Novel;
 import com.enigma.enigpus.service.InventoryService;
 import com.enigma.enigpus.util.Utility;
@@ -19,9 +20,9 @@ public class BookView {
 
     public void mainMenu() {
         while (isValid) {
-            System.out.println("-".repeat(100));
+            System.out.println("-".repeat(120));
             System.out.println("Enigma Perpustakaan");
-            System.out.println("-".repeat(100));
+            System.out.println("-".repeat(120));
             System.out.print("""
                     1. Tambah Data Buku
                     2. Lihat Semua Data Buku
@@ -29,7 +30,7 @@ public class BookView {
                     4. Hapus Data Buku
                     X. Keluar
                     """);
-            System.out.println("-".repeat(100));
+            System.out.println("-".repeat(120));
             System.out.print("Input Menu: ");
             String inputMenu = Utility.inputStr();
             System.out.println();
@@ -52,13 +53,28 @@ public class BookView {
 
     private void viewAllBook() {
         List books = inventoryService.getAllBook();
-        System.out.println("Buku Novel");
-        String format = String.format("%-10s %-30s %-30s %-30s", "Kode", "Judul", "Penerbit", "Tahun Terbit");
+        System.out.println("Data Buku Novel: ");
+        String formatNovel = String.format("%-10s %-30s %-30s %-30s %-30s", "Kode", "Judul", "Penulis", "Penerbit", "Tahun Terbit");
 
-        System.out.println(format);
+        System.out.println(formatNovel);
         for (Object book : books) {
-            Novel novel = (Novel) book;
-            System.out.printf("%-10s %-30s %-30s %-30s", novel.getCode(), novel.getTitle(), novel.getPublisher(), novel.getPublisher());
+            if (book instanceof Novel) {
+                Novel novel = (Novel) book;
+                System.out.printf("%-10s %-30s %-30s %-30s %-30s", novel.getCode(), novel.getTitle(), novel.getWriter(), novel.getPublisher(), novel.getReleaseYear());
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.println("Data Majalah: ");
+        String formatMagazine = String.format("%-10s %-30s %-30s %-30s", "Kode", "Judul", "Penerbit", "Tahun Terbit");
+
+        System.out.print(formatMagazine);
+        for (Object book : books) {
+            if (book instanceof Magazine) {
+            Magazine magazine = (Magazine) book;
+            System.out.printf("%-10s %-30s %-30s %-30s", magazine.getCode(), magazine.getTitle(), magazine.getReleasePer(), magazine.getReleaseYear());
+            }
             System.out.println();
         }
         System.out.println();
@@ -73,16 +89,19 @@ public class BookView {
                     2. Tambah Majalah
                     0. Kembali
                     """);
-            System.out.println("-".repeat(100));
+            System.out.println("-".repeat(120));
             System.out.print("Input Menu: ");
             String bookType = Utility.inputStr();
             System.out.println();
+
             switch (bookType) {
                 case "1":
                     saveNovelView();
                     mainMenu();
                     return;
                 case "2":
+                    saveMagazineView();
+                    mainMenu();
                     return;
                 case "0":
                     mainMenu();
@@ -91,8 +110,25 @@ public class BookView {
         }
     }
 
+    private void saveMagazineView() {
+        String code = Utility.incrementNovelCode();
+
+        System.out.print("Input Judul: ");
+        String title = Utility.inputStr();
+        System.out.print("Input Periode Terbit (Monthly/Yearly): ");
+        String releasePer = Utility.inputStr();
+        System.out.print("Input Tahun Terbit: ");
+        int year = Utility.inputInt();
+
+        Book magazine = new Magazine(code, title, releasePer, year);
+        inventoryService.addBook(magazine);
+        System.out.println(magazine);
+
+    }
+
     private void saveNovelView() {
         String code = Utility.incrementNovelCode();
+
         System.out.print("Input Judul: ");
         String title = Utility.inputStr();
         System.out.print("Input Penerbit: ");
